8a1682d4e81269dc10c8d067ae4f4903bad3d0e1
imvickykumar999 <imvickykumar999@gmail.com>
2022-12-18 22:49:31 +0530

    ab kal krenge

--- video_colorization.py
+++ video_colorization.py
@@ -1,36 +1,38 @@
 
 # # python video_colorization.py -i input.jpg -o output.jpg
 
-# import argparse
-# import matplotlib.pyplot as plt
-# from colorizers import *
+import argparse
+import matplotlib.pyplot as plt
+from colorizers import *
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-i','--img_path', type=str, default='imgs/ansel_adams3.jpg')
-# parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
-# parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
-# opt = parser.parse_args()
+def magic(input_path, output_path):
+	parser = argparse.ArgumentParser()
+	# parser.add_argument('-i','--img_path', type=str, default='imgs/ansel_adams3.jpg')
+	parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
+	# parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
+	opt = parser.parse_args()
 
-# colorizer_eccv16 = eccv16(pretrained=True).eval()
-# colorizer_siggraph17 = siggraph17(pretrained=True).eval()
-# if(opt.use_gpu):
-# 	colorizer_eccv16.cuda()
-# 	colorizer_siggraph17.cuda()
+	colorizer_eccv16 = eccv16(pretrained=True).eval()
+	colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+	if(opt.use_gpu):
+		colorizer_eccv16.cuda()
+		colorizer_siggraph17.cuda()
 
-# img = load_img('vid/%s'%opt.img_path)
-# (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
-# if(opt.use_gpu):
-# 	tens_l_rs = tens_l_rs.cuda()
+	img = load_img(input_path)
+	(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
+	if(opt.use_gpu):
+		tens_l_rs = tens_l_rs.cuda()
 
-# img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig), dim=1))
+	img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig), dim=1))
+	out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+	plt.imsave(output_path, out_img_eccv16)
 
-# out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
-# plt.imsave('vid_out/output_eccv16.png', out_img_eccv16)
-
-# # out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
-# # plt.imsave('vid_out/output_siggraph17.png', out_img_siggraph17)
+	# out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+	# plt.imsave('vid_out/output_siggraph17.png', out_img_siggraph17)
 
 # ------------------------------
+
+# video into frame
 
 # # https://stackoverflow.com/questions/33311153/python-extracting-and-saving-video-frames/33399711#33399711
 
@@ -46,6 +48,8 @@
 #   count += 1
 
 # ---------------------------------
+
+# merge frame into video
 
 # https://www.geeksforgeeks.org/python-create-video-using-multiple-images-using-opencv/
 
@@ -103,52 +107,57 @@
 
 # generate_video()
 
+# -----------------------------------
+
+# waiting 
+
 # import time
 # print("Print now")
 # time.sleep(2)
 # print("Printing after 2 seconds")
 
+# ----------------------------------------
+
+# move file
+
+# import os
+# os.replace("vid_out/mygeneratedvideo.avi", "vid/mygeneratedvideo.avi")
+
+# ---------------------------------
+
+# video speed change
 
 # https://stackoverflow.com/questions/63631973/how-can-i-use-python-to-speed-up-a-video-without-dropping-frames/63632689#63632689
 
-from moviepy.editor import VideoFileClip
-import moviepy.video.fx.all as vfx
+# from moviepy.editor import VideoFileClip, AudioFileClip
+# import moviepy.video.fx.all as vfx
 
-in_loc = 'vid_out/mygeneratedvideo.avi'
-out_loc = 'vid/dummy_out.mp4'
+# in_loc = 'vid/mygeneratedvideo.avi'
+# out_loc = 'vid/dummy_out.mp4'
 
-# Import video clip
-clip = VideoFileClip(in_loc)
-print("fps: {}".format(clip.fps))
+# # Import video clip
+# clip = VideoFileClip(in_loc)
+# print("fps: {}".format(clip.fps))
 
-# Modify the FPS
-clip = clip.set_fps(clip.fps * 3)
+# # Modify the FPS
+# clip = clip.set_fps(clip.fps * 3)
 
-# Apply speed up
-final = clip.fx(vfx.speedx, 25)
-print("fps: {}".format(final.fps))
+# # Apply speed up
+# final = clip.fx(vfx.speedx, 25)
+# print("fps: {}".format(final.fps))
 
-# Save video clip
-final.write_videofile(out_loc)
+# # getting only first 5 seconds
+# # clip = clip.subclip(0, 5)
 
-# -----------------------------------
+# # https://zulko.github.io/moviepy/getting_started/audioclips.html
+# # loading audio file
+# audioclip = AudioFileClip("vid/big_buck_bunny_720p_5mb.mp4")
 
-# Import everything needed to edit video clips
-from moviepy.editor import *
+# # adding audio to the video clip
+# videoclip = final.set_audio(audioclip)
 
+# # showing video clip
+# # videoclip.ipython_display()
 
-# loading video dsa gfg intro video
-clip = VideoFileClip("dsa_geek.mp4")
-
-
-# getting only first 5 seconds
-clip = clip.subclip(0, 5)
-
-# loading audio file
-audioclip = AudioFileClip("allwell.mp3").subclip(0, 5)
-
-# adding audio to the video clip
-videoclip = clip.set_audio(audioclip)
-
-# showing video clip
-videoclip.ipython_display()
+# # Save video clip
+# videoclip.write_videofile(out_loc)
