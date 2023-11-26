8ef783b1e949c107931758decbb7f14043cf8181
Sebastian Regalado <sdregala@uwaterloo.ca>
2023-11-25 22:18:02 -0500

    remove test file

--- dataset/test.py
+++
@@ -1,9 +0,0 @@
-import joblib
-# from dataset.cielab import CIELabConversion
-
-# cielab = CIELabConversion(
-#     buckets_path="resources/buckets_313.npy",
-#     buckets_knn_path="resources/buckets_knn.joblib"
-# )
-
-buckets_knn = joblib.load("resources/buckets_knn.joblib")
