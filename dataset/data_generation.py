72d361983a0ff3a5fc5f68e6cd4532a934677199
Chun Kang Lu <chunkanglu@gmail.com>
2023-11-25 20:10:16 -0500

    modify data generation adding bucket labels

--- dataset/data_generation.py
+++ dataset/data_generation.py
@@ -1,24 +1,38 @@
 
 import argparse
+import numpy as np
 from pathlib import Path
 from skimage.color import rgb2gray
 from skimage.io import imread, imsave
 from skimage.util import img_as_ubyte
 from tqdm import tqdm
 
+from dataset.cielab import CIELabConversion
+
 def create_dataset(
     images_path: str,
     output_path: str,
+    buckets_path: str = "resources/buckets_313.npy",
+    buckets_knn_path: str = "resources/buckets_knn.joblib",
     grayscale_name_prefix: str = "gray",
-    color_name_prefix: str = "color"
+    color_name_prefix: str = "color",
+    bucket_label_prefix: str = "bucket"
 ) -> None:
+    cielab = CIELabConversion(
+        buckets_path=buckets_path,
+        buckets_knn_path=buckets_knn_path
+    )
+
     images_path = Path(images_path)
     output_path = Path(output_path)
     grayscale_path = output_path.joinpath(grayscale_name_prefix)
     color_path = output_path.joinpath(color_name_prefix)
+    bucket_labels_path = output_path.joinpath(bucket_label_prefix)
+
     try:
         grayscale_path.mkdir(parents=True, exist_ok=False)
         color_path.mkdir(parents=True, exist_ok=False)
+        bucket_labels_path.mkdir(parents=True, exist_ok=False)
     except FileExistsError:
         print("Dataset folder already exists.")
         return
@@ -39,11 +53,21 @@ def create_dataset(
                                     image_path.stem).absolute().as_posix() +
             ".png"
         )
+        bucket_label_output = (
+            bucket_labels_path.joinpath(bucket_label_prefix +
+                                    "_" +
+                                    image_path.stem).absolute().as_posix() +
+            ".npy"
+        )
         imsave(color_image_output, image)
+
+        np.save(bucket_label_output, cielab.get_image_ab_buckets(image))
+
         # Convert from float grayscale format to uint 0-255 format
         # NOTE: this changes the values ever so slightly but is not significant
         image = img_as_ubyte(rgb2gray(image))
         imsave(grayscale_image_output, image)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -51,9 +75,15 @@ if __name__ == "__main__":
     parser.add_argument("output_path", help="Path to output dataset to")
     parser.add_argument("-g", "--gray_prefix", default="gray", help="Name prefix for grayscale output images")
     parser.add_argument("-c", "--color_prefix", default="color", help="Name prefix for color output images")
+    parser.add_argument("-b", "--bucket_prefix", default="bucket", help="Name prefix for image bucket labels")
+    parser.add_argument("--buckets_path", default="resources/buckets_313.npy", help="Path to quantized ab-value buckets")
+    parser.add_argument("--buckets_knn_path", default="resources/buckets_knn.joblib", help="Path to buckets KNN")
     args = parser.parse_args()
 
     create_dataset(args.input_path,
                    args.output_path,
+                   args.buckets_path,
+                   args.buckets_knn_path,
                    args.gray_prefix,
-                   args.color_prefix)
+                   args.color_prefix,
+                   args.bucket_prefix)
--- dataset/dataset.py
+++ dataset/dataset.py
@@ -18,18 +18,22 @@ class ColorizationDataset(Dataset):
         cielab_conversion: CIELabConversion = CIELabConversion(),
         grayscale_name_prefix: str = "gray",
         color_name_prefix: str = "color",
+        bucket_label_prefix: str = "bucket",
         resize_image_size: T.Union[T.Tuple[int, int], None] = (256, 256)
     ) -> None:
         self.grayscale_name_prefix = grayscale_name_prefix
         self.color_name_prefix = color_name_prefix
+        self.bucket_label_prefix = bucket_label_prefix
 
         self.grayscale_path = os.path.join(dataset_path, grayscale_name_prefix)
         self.color_path = os.path.join(dataset_path, color_name_prefix)
+        self.bucket_path = os.path.join(dataset_path, bucket_label_prefix)
+
         # NOTE: This may need to change if we can't load all filenames in memory
         self.grayscale_images = os.listdir(self.grayscale_path)
         self.color_images = os.listdir(self.color_path)
+        self.bucket_labels = os.listdir(self.bucket_path)
 
-        self.cielab = cielab_conversion
         self.resize_image_size = resize_image_size
 
     def __len__(self) -> int:
@@ -40,6 +44,10 @@ class ColorizationDataset(Dataset):
         color_image_path = (self.color_name_prefix +
                             "_" +
                             grayscale_image_path.removeprefix(self.grayscale_name_prefix + "_"))
+        # There's probably a better way to do this
+        bucket_label_path = (self.bucket_label_prefix +
+                             "_" +
+                             grayscale_image_path.removeprefix(self.grayscale_name_prefix + "_"))[:-4] + ".npy"
         
         grayscale_image = imread(os.path.join(self.grayscale_path, grayscale_image_path), as_gray=True)
         if self.resize_image_size:
@@ -51,7 +59,7 @@ class ColorizationDataset(Dataset):
             color_image = resize_image(color_image, shape=self.resize_image_size)
         color_image = rgb2lab(color_image)
 
-        bucket_ids = self.cielab.get_image_ab_buckets(color_image)
+        bucket_ids = np.load(os.path.join(self.bucket_path, bucket_label_path))
 
         # TODO: Preprocessing
 
